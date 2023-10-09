7116cd14fec23766dc87e6a8158e31bc596eed1e
Srikanth Reddy <bizisree@outlook.com>
2023-10-09 22:12:21 +0100

    First commit

---
+++ provider.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+terraform {
+  required_version = "<= 1.6.0" #Forcing which version of Terraform needs to be used
+  required_providers {
+    aws = {
+      version = "<= 6.0.0" #Forcing which version of plugin needs to be used.
+      source  = "hashicorp/aws"
+    }
+  # }
+  # backend "s3" {
+  #   bucket         = "devsecopsb35-tfstate"
+  #   key            = "ansible.tfstate"
+  #   region         = "us-east-1"
+  #   dynamodb_table = "devsecopsb35-state-locking"
+  #   encrypt        = true
+  # }
+}
